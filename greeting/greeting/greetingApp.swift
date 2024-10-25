28e6250f32ce531df51da074ed63c8e92481e658
Mike Sears <mike.sears@marqueone.net>
2024-10-24 20:22:18 -0700

    first commit

---
+++ greeting/greeting/greetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  greetingApp.swift
+//  greeting
+//
+//  Created by Mike on 2024-10-24.
+//
+
+import SwiftUI
+
+@main
+struct greetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
