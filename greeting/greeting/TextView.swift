06e2d89e9368318cd401049b6a417052b1659dd5
Mike Sears <mike.sears@marqueone.net>
2024-11-04 19:11:19 -0800

    adding state change and animation on button click

--- greeting/greeting/TextView.swift
+++ greeting/greeting/TextView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct TextView: View {
+    var _defaultColor: Color = .red;
     let text: String
-    let color: Color
+    @State var color: Color
+    
+    let colors: [Color] = [
+        .red,
+        .green,
+        .blue,
+        .orange,
+        .yellow,
+        .purple,
+        Color(red: 0.5, green: 0, blue: 0.5),
+        Color(red: 0, green: 0.5, blue: 0.5),
+        Color(red: 139/255, green: 207/255, blue: 240/255),
+        Color(red: 1, green: 215/255, blue: 0)
+    ]
     
     var body: some View {
         Text(text)
@@ -19,6 +33,11 @@ struct TextView: View {
             .background(color.opacity(0.4))
             .cornerRadius(20)
             .shadow(color: color, radius: 5, x: 10, y: 10)
+            .onTapGesture {
+                withAnimation {
+                    color = colors.randomElement() ?? _defaultColor
+                }
+            }
        
     }
 }
